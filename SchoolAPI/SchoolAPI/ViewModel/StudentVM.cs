a5a3cc5e948fc1718a484da72465b2415d416f69
Anderson Silva <anderson.silva@elisbrasil.onmicrosoft.com>
2025-04-16 17:54:48 -0300

    created school api

---
+++ SchoolAPI/SchoolAPI/ViewModel/StudentVM.cs
@@ -0,0 +1,11 @@
+﻿namespace SchoolAPI.ViewModel
+{
+    public class StudentVM
+    {
+        public Guid Id { get; set; }
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public string Course { get; set; }
+        public int Score { get; set; }
+    }
+}
